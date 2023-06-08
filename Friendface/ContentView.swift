697479aea2fd04712f61c607fc2bbdcf70e7b414
Uriel Ortega <urielortega2509@gmail.com>
2023-06-08 16:23:47 -0600

    Created structs for entities and function to load data from URL.

--- Friendface/ContentView.swift
+++ Friendface/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var users = [User]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List(users) { user in
+                VStack(alignment: .leading) {
+                    Text(user.name)
+                        .font(.headline)
+                    Text(user.isActive ? "Online 👋🏼" : "Offline 💤")
+                }
+                .padding(3)
+            }
+            .navigationTitle("Friendface")
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        // Creating the URL we want to read.
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            // Fetching the data for the URL.
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // Decoding the result of the data into a User array
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            
+            if let decodedResponse = try? decoder.decode([User].self, from: data) {
+                users = decodedResponse
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
---
+++ Friendface/User.swift
@@ -0,0 +1,31 @@
+//
+//  User.swift
+//  Friendface
+//
+//  Created by Uriel Ortega on 08/06/23.
+//
+
+import Foundation
+
+struct User: Codable, Identifiable {
+    struct Friend: Codable {
+        var id: String
+        var name: String
+    }
+    
+    var id: String
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var about: String
+    var registered: Date
+    var tags: [String]
+    var friends: [Friend]
+    
+    var formattedRegisteredDate: String {
+        registered.formatted(date: .abbreviated, time: .omitted)
+    }
+}
