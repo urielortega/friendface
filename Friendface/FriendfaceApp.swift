4bf9134d68a40ff97281a902840e442a1a8655ea
Uriel Ortega <urielortega2509@gmail.com>
2023-06-08 11:21:27 -0600

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Uriel Ortega on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
